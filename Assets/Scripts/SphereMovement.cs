731b1ee412672cb3059aa1dfc57c4743867c10ab
nico <32666723+draco1703@users.noreply.github.com>
2019-01-31 00:26:22 +0100

    Add files via upload

---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+    }
+}
---
+++ Assets/Scripts/buttonscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonscript : MonoBehaviour
+{
+    public string _printtext;
+
+    public void printText()
+    {
+        print(_printtext);
+    }
+    public void PlayAudio()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+  
+        public Transform prefab;
+    public void instansiateBluecube()
+    {
+        Instantiate(prefab, (transform.position), Quaternion.identity);
+
+    }
+    
+}
+
